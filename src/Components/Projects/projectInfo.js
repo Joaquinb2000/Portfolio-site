cd00c8e591d010c32ba8f0e0b6a12419f400918a
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-02-19 04:14:30 -0400

    Added new projects

--- src/Components/Projects/projectInfo.js
+++ src/Components/Projects/projectInfo.js
@@ -5,7 +5,35 @@ const projectInfo= [
         description: "The app accepts image urls and uses the Clarifai detection API to identify faces in the image. It handles user interaction with its own server. It also counts with its own database for user registration and login",
         img: "https://i.imgur.com/k56Ys4s.png",
         used: "Javascript, React, Node (using Express), and PostgreSQL",
-        link: "https://face-detect-app-jb.herokuapp.com/"
+        link: "https://joaquinb2000.github.io/face-recognition-app/"
+    },
+    {
+        name: "Message Me",
+        description: "Real time chatroom application, users can create and edit chatroom members and title",
+        img: "https://i.imgur.com/G3GAG43.png",
+        used: "Ruby on Rails, PostgreSQL, Redis, Bootstrap",
+        link: "https://messageme-jb.fly.dev/"
+    },
+    {
+        name: "Site as a Service app",
+        description: "Organizations can manage artifacts (files stored in AWS S3 bucket), and projects. Organizations can create employee accounts by sending the new member an email with a one time link which will allow the user to finish creating their account",
+        img: "https://imgur.com/sKp2v8y.png",
+        used: "Ruby on Rails, PostgreSQL, Auth with Devise and Milia, file upload handled with AWS, Bootsrap",
+        link: "https://jb-saas-app.fly.dev/"
+    },
+    {
+        name: "Photo-App",
+        description: "Image sharing site",
+        img: "https://imgur.com/rjKXot1.png",
+        used: "Ruby on Rails, Auth with Devise, image upload handled with AWS S3 bucket",
+        link: "https://photo-app.fly.dev/"
+    },
+    {
+        name: "University App",
+        description: "Mockup for a university site with a listing for courses, information page, a student index, profiles",
+        img: "https://imgur.com/4LfHlla.png",
+        used: "Ruby on Rails, PostgreSQL, Auth with Devise",
+        link: "https://university-app.fly.dev/"
     },
     {
         name:"Robofriends",
@@ -13,7 +41,7 @@ const projectInfo= [
         img: "https://i.imgur.com/Dz1lEPW.png",
         used: "React",
         link: "https://joaquinb2000.github.io/robofriends/"
-    }
+    },
 ]
 
 export default projectInfo;
---
+++ src/Components/SocialMedia/Accounts.js
@@ -0,0 +1,15 @@
+import Github from './img/PngItem_1280311.png';
+import Linkedin from "./img/pngkey.com-linkedin-logo-png-290376.png";
+
+export const SOCIAL_MEDIA = [
+    {
+        href: "https://www.linkedin.com/in/joaquin-berrios-826001172/",
+        imgSrc: Linkedin,
+        alt: "linkedin"
+    },
+    {
+        href: "https://github.com/Joaquinb2000",
+        imgSrc: Github,
+        alt: "github"
+    },
+]

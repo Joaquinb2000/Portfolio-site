4d7aba1cd5427fd7ed8bdbffc4160e37484802fa
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-02-20 00:02:18 -0400

    changing descriptions in some projects

--- src/Components/Projects/projectInfo.js
+++ src/Components/Projects/projectInfo.js
@@ -2,7 +2,7 @@
 const projectInfo= [
     {
         name:"Face Detect",
-        description: "The app accepts image urls and uses the Clarifai detection API to identify faces in the image. It handles user interaction with its own server. It also counts with its own database for user registration and login",
+        description: "The app accepts image urls and uses the Clarifai detection API to identify faces in the image. It handles user interaction with its own server. It also counts with its own database for user registration and login.",
         img: "https://i.imgur.com/k56Ys4s.png",
         used: "Javascript, React, Node (using Express), and PostgreSQL",
         link: "https://joaquinb2000.github.io/face-recognition-app/"
@@ -16,7 +16,7 @@ const projectInfo= [
     },
     {
         name: "Site as a Service app",
-        description: "Organizations can manage artifacts (files stored in AWS S3 bucket), and projects. Organizations can create employee accounts by sending the new member an email with a one time link which will allow the user to finish creating their account",
+        description: "Organizations can manage projects, employees, and artifacts (files stored in AWS S3 bucket). Implemented a subscription payment system with stripe. Organizations can create employee accounts by sending the new member an email with a one time link which will allow the user to finish creating their account.",
         img: "https://imgur.com/sKp2v8y.png",
         used: "Ruby on Rails, PostgreSQL, Auth with Devise and Milia, file upload handled with AWS, Bootsrap",
         link: "https://jb-saas-app.fly.dev/"
@@ -25,19 +25,19 @@ const projectInfo= [
         name: "Photo-App",
         description: "Image sharing site",
         img: "https://imgur.com/rjKXot1.png",
-        used: "Ruby on Rails, Auth with Devise, image upload handled with AWS S3 bucket",
+        used: "Ruby on Rails, Auth with Devise, image upload handled with AWS S3 bucket, implemented subcription plan payments with Stripe API.",
         link: "https://photo-app.fly.dev/"
     },
     {
         name: "University App",
-        description: "Mockup for a university site with a listing for courses, information page, a student index, profiles",
+        description: "Mockup for a university site with a listing for courses, information page, student index and profiles.",
         img: "https://imgur.com/4LfHlla.png",
         used: "Ruby on Rails, PostgreSQL, Auth with Devise",
         link: "https://university-app.fly.dev/"
     },
     {
         name:"Robofriends",
-        description: "Simple showcase of React",
+        description: "Simple showcase of React.",
         img: "https://i.imgur.com/Dz1lEPW.png",
         used: "React",
         link: "https://joaquinb2000.github.io/robofriends/"
